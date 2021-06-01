4805e6794242b05f44c12e1ed634deb1ae0dd832
Talebi <rtalebi3@eosl.gtri.org>
2021-06-01 10:06:58 -0400

    some debugging files added

---
+++ drmaa_test.py
@@ -0,0 +1,34 @@
+'''
+quick file for testing drmaa stuff
+
+going to follow the examples in the drmaa tutorial
+https://drmaa-python.readthedocs.io/en/latest/tutorials.html
+'''
+import os
+import sys
+import time
+import drmaa
+
+
+with drmaa.Session as s:
+    jt = s.createJobTemplate()
+    jt.remoteCommand = os.path.join(os.getcwd(), 'run_simple.sh')
+    jt.args = []
+    jt.joinFiles = True
+    jobid = s.runJob(jt)
+    print('Your job has been submitted with ID %s' % jobid)
+
+    #start = time.time()
+    for _ in range(20):
+        print('\tChecking status -> %s' % (s.jobStatus(jobid)))
+
+    print('Cleaning up')
+    s.deleteJobTemplate(jt)
+
+print("Did job succeed?")
+if os.path.exists("simple_test.txt"):
+    print("\tSUCCESS!")
+else:
+    print("\tFAILED.")
+
+print("") #empty line to close
---
+++ run_simple.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# sleep_time=$(((RANDOM%181)))
+sleep_time=7 # "$1"
+output_file="simple_test.txt" #"$2"
+
+sleep $sleep_time
+echo "$sleep_time" >> $output_file
